d8acc90cf487dda8a3a7f649103e174d57c441a9
NetologtTest <pvsaep.vaapa@yandex.ru>
2023-03-29 10:11:11 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+        System.out.println(bins.sum("1", "0"));
+        System.out.println(bins.mult("101", "11"));
+    }
+}
